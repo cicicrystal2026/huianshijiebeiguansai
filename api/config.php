bd6cea6cef6b21a41f67f44250777ab25b013805
cicicrystal2026 <cicicrystal607@gmail.com>
2026-07-10 20:51:25 +0800

    Add files via upload
    Add PHP signup backend

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$SIGNUP_ALLOWED_ORIGINS = array(
+    'https://mjm.chindaliy.com.cn',
+    'http://mjm.chindaliy.com.cn',
+    'http://127.0.0.1:8788',
+    'http://localhost:8788',
+);
+
+define('SIGNUP_EXPORT_TOKEN', 'Huian2026_change_this_token');
